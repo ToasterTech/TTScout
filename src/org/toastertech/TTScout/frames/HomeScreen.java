4581735bae34ca9336f2dd79517bd93cb2511e93
Jonathan Daniel <jonathandaniel@fernbanklinks.com>
2017-12-02 16:52:58 -0500

    Started Home Screen

---
+++ src/org/toastertech/TTScout/frames/HomeScreen.java
@@ -0,0 +1,15 @@
+package org.toastertech.TTScout.frames;
+
+import javax.swing.*;
+
+public class HomeScreen extends JFrame {
+
+
+    public HomeScreen(){
+        setVisible(true);
+        setSize(800,800);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+}
---
+++ src/org/toastertech/TTScout/main/Main.java
@@ -0,0 +1,10 @@
+package org.toastertech.TTScout.main;
+
+import org.toastertech.TTScout.frames.HomeScreen;
+
+public class Main {
+
+    public static void main(String[] args){
+        new HomeScreen();
+    }
+}
