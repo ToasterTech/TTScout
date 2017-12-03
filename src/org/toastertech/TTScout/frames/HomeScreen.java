136490554e5e743e34b8a7dbd777c297eaf1ac14
Jonathan Daniel <gcagrad19@gmail.com>
2017-12-02 21:01:27 -0500

    Tried to Get Logo Working, Still not Loading

--- src/org/toastertech/TTScout/frames/HomeScreen.java
+++ src/org/toastertech/TTScout/frames/HomeScreen.java
@@ -1,15 +1,61 @@
 package org.toastertech.TTScout.frames;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class HomeScreen extends JFrame {
+    JLabel logo = new JLabel();
 
+
+
+    JPanel buttonPanel = new JPanel(new FlowLayout());
 
     public HomeScreen(){
-        setVisible(true);
-        setSize(800,800);
+        System.out.println(new File(".").getAbsolutePath());
+        //Handling the Basic Background
+        setVisible(true); //Display
+        setSize(800,800); //Screen Size - Should be Resizable
+        setDefaultCloseOperation(HIDE_ON_CLOSE);
+        setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
 
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+
+        setupButtonPanel();
+        add(buttonPanel);
+
+        try {
+            setupTeamLogo();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        //Adding Components
+        add(logo);
     }
+
+    private void setupTeamLogo() throws Exception{
+        BufferedImage bufferedImage = ImageIO.read(this.getClass().getClassLoader().getResource("org/toastertech/TTScout/res/ttlogo.png"));
+        Image image = bufferedImage.getScaledInstance(400,400, Image.SCALE_SMOOTH);
+
+        logo.setIcon(new ImageIcon(image));
+
+
+    }
+
+    private void setupButtonPanel(){
+        JButton oldFilesButton, newFileButton;
+
+        oldFilesButton = new JButton("Select Saved Match");
+        newFileButton = new JButton("Create New Match");
+
+        buttonPanel.add(oldFilesButton);
+        buttonPanel.add(newFileButton);
+
+        add(buttonPanel);
+    }
+
 
 }
