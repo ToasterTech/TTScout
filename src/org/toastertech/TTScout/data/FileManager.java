0642a00f0e77c1155a211686665c0cde0d87f8a2
Jonathan Daniel <gcagrad19@gmail.com>
2017-12-06 17:54:54 -0500

    File Manager Class
    This class is what we will call all of our read/write functions from.

---
+++ src/org/toastertech/TTScout/data/FileManager.java
@@ -0,0 +1,16 @@
+package org.toastertech.TTScout.data;
+
+/**
+ * This will essentially be a static class that we will call read/write functions from.
+ */
+public class FileManager {
+
+    /**
+     * Constructor is private so we can't make instances of this. Instead, we will cass our functions
+     * with FileManager.method() vs. creating multiple instances to juggle.
+     */
+    private FileManager(){
+
+    }
+
+}
