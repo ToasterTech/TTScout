a4c9fabf6732bb72c5a1210b50986ed08fbcef18
Jonathan Daniel <gcagrad19@gmail.com>
2017-12-11 17:40:17 -0500

    Select Event Name
    Replaced Event Text Box with Alert to select event name to increase consistency

--- src/org/toastertech/TTScout/panels/CreateMatchPanel.java
+++ src/org/toastertech/TTScout/panels/CreateMatchPanel.java
@@ -1,11 +1,18 @@
-package org.toastertech.TTScout.frames;
+package org.toastertech.TTScout.panels;
 
 
 import org.toastertech.TTScout.data.Match;
-
 import javax.swing.*;
+import org.supercsv.cellprocessor.*;
+import org.toastertech.TTScout.frames.HomeScreen;
 
-public class CreateMatchPanel extends JPanel{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * This JPanel will be used by the user to create a match.
+ */
+public class CreateMatchPanel extends JPanel implements ActionListener{
     JLabel     matchNumLabel = new JLabel("Match Number: "),
               teamNumLabel   = new JLabel("Team Number: "),
               scoutNameLabel = new JLabel("   Scout Name: "),
@@ -22,9 +29,8 @@ public class CreateMatchPanel extends JPanel{
                 scoutNamePanel = new JPanel(),
                 eventNamePanel = new JPanel();
 
-    JButton submitButton = new JButton("Submit");
-
-
+    JButton submitButton = new JButton("Submit"),
+            selectEventButton = new JButton("Select Event");
 
     public CreateMatchPanel(HomeScreen homeScreen){
         // Setup
@@ -55,8 +61,11 @@ public class CreateMatchPanel extends JPanel{
         scoutNamePanel.add(scoutNameField);
 
         eventNamePanel.add(eventNameLabel);
-        eventNameField.setColumns(5);
-        eventNamePanel.add(eventNameField);
+        //eventNameField.setColumns(5);
+        //eventNamePanel.add(eventNameField);
+        selectEventButton.addActionListener(this);
+        eventNamePanel.add(selectEventButton);
+
 
         add(matchNumPanel);
         add(teamNumPanel);
@@ -64,13 +73,20 @@ public class CreateMatchPanel extends JPanel{
         add(eventNamePanel);
     }
 
+    /**
+     * This lets us know if the text entered in the boxes is data we can work with. If we can't work with this
+     * data, the method returns false.
+     *
+     * @return
+     */
     public boolean textIsValid(){
         try{ //This will throw an error if we can't get a valid input
-            int matchNum = Integer.valueOf(matchNumField.getText().toString());
-            int teamNum = Integer.valueOf(teamNumField.getText().toString());
+            Integer.valueOf(matchNumField.getText().toString()); //Are there characters that are not numbers?
+            Integer.valueOf(teamNumField.getText().toString()); // Same Question
 
+            //Is there nothing in either of these fields?
             if(scoutNameField.getText().length() <= 0 || eventNameField.getText().length() <= 0){
-                throw new Exception();
+                throw new Exception(); //This will throw an exception, letting us know that we need to change the input.
             }
         } catch(Exception e){
             System.out.println(e.getMessage());
@@ -79,10 +95,23 @@ public class CreateMatchPanel extends JPanel{
         return true;
     }
 
+    /**
+     * Return an empty match object for the user to work with. All it contains is the data inputted in this panel.
+     * @return
+     */
     public Match getMatch(){
         return new Match(Integer.valueOf(matchNumField.getText().toString()), Integer.valueOf(teamNumField.getText().toString()),
-                        scoutNameField.getText(), eventNameField.getText());
+                        scoutNameField.getText(), selectEventButton.getText());
 
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand() == selectEventButton.getActionCommand()){
+            String[] competitions = {"Gainesville", "Dalton", "Albany", "Columbus", "Duluth", "State", "World's"};
+            selectEventButton.setText((String) JOptionPane.showInputDialog(null, "", "Select an Event: ",
+                    JOptionPane.QUESTION_MESSAGE, null,
+                    competitions, competitions[2]));
+        }
+    }
 }
