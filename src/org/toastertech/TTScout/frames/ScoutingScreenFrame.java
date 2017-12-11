a7f5ba070e8f8f72b6bd676c1eff98ca9b10b897
Jonathan Daniel <jonathandaniel@fernbanklinks.com>
2017-12-11 17:53:36 -0500

    Created Match Details Panel
    This panel will contain the details for the match

--- src/org/toastertech/TTScout/frames/ScoutingScreenFrame.java
+++ src/org/toastertech/TTScout/frames/ScoutingScreenFrame.java
@@ -1,16 +1,22 @@
 package org.toastertech.TTScout.frames;
 
 import org.toastertech.TTScout.data.Match;
+import org.toastertech.TTScout.panels.MatchDetailsPanel;
+import org.toastertech.TTScout.panels.RatingPanel;
 
 import javax.swing.*;
 
 public class ScoutingScreenFrame extends JFrame{
+    Match currentMatch;
 
     public ScoutingScreenFrame(Match match){
             setSize(800, 800);
             setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
             setVisible(true);
 
+            currentMatch = match;
+            add(new MatchDetailsPanel(match));
+            add(new RatingPanel(0, 10, "Test Slider"));
 
     }
 
--- src/org/toastertech/TTScout/panels/MatchDetailsPanel.java
+++ src/org/toastertech/TTScout/panels/MatchDetailsPanel.java
@@ -1,7 +1,42 @@
 package org.toastertech.TTScout.panels;
 
+import org.toastertech.TTScout.data.Match;
+
 import javax.swing.*;
 
 public class MatchDetailsPanel extends JPanel {
+    JLabel matchNumLabel,
+            teamNumLabel,
+            scoutNameLabel;
 
+    JTextField matchNumField,
+            teamNumField,
+            scoutNameField;
+
+    JButton competitionButton;
+
+    public MatchDetailsPanel(Match match){
+        matchNumLabel  = new JLabel("Match Number: ");
+        teamNumLabel   = new JLabel("Team Number: ");
+        scoutNameLabel = new JLabel("Scout Name: ");
+
+        matchNumField  = new JTextField();
+        teamNumField   = new JTextField();
+        scoutNameField = new JTextField();
+
+        matchNumField  .setText(String.valueOf(match.getMatchNum()));
+        teamNumField   .setText(String.valueOf(match.getTeamNum()));
+        scoutNameField .setText(match.getScoutName());
+
+        matchNumField.setColumns(5);
+        teamNumField.setColumns(5);
+        scoutNameField.setColumns(5);
+
+        add(matchNumLabel);
+        add(matchNumField);
+        add(scoutNameLabel);
+        add(scoutNameField);
+        add(teamNumLabel);
+        add(teamNumField);
+    }
 }
