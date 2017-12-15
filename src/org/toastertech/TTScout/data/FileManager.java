3c2f8ad00fa9d2d6eefd2db183eb81f4a5954d0f
Jonathan Daniel <gcagrad19@gmail.com>
2017-12-14 23:11:04 -0500

    File Manager Class File I/O Methods
    Added in the read and write methods for our app.

--- src/org/toastertech/TTScout/data/FileManager.java
+++ src/org/toastertech/TTScout/data/FileManager.java
@@ -1,9 +1,43 @@
 package org.toastertech.TTScout.data;
 
+import org.supercsv.cellprocessor.ConvertNullTo;
+import org.supercsv.cellprocessor.ParseInt;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvBeanReader;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanReader;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
 /**
  * This will essentially be a static class that we will call read/write functions from.
  */
 public class FileManager {
+    /**
+     * This is pretty much a list of all the matches we are working with at the moment.
+     */
+    public static ArrayList<Match> currentMatches = new ArrayList<>();
+
+    /**
+     * This represents the save directory for out files and our current save file.
+     */
+    public static File currentDirectory,
+                       currentFile;
+
+    /**
+     * This is just the name of the competition we are scouting for.
+     */
+    public static String currentCompetition;
+
+    static String[] spreadsheetHeaders = {"Match Number", "Team Number", "Scout Name"};
+    static String[] parameterNames = {"matchNum", "teamNum", "scoutName"};
+    static CellProcessor[] processors = {new ParseInt(), new ParseInt(), new ConvertNullTo("")};
 
     /**
      * Constructor is private so we can't make instances of this. Instead, we will cass our functions
@@ -13,4 +47,60 @@ public class FileManager {
 
     }
 
+    /**
+     * This will pretty much create our directory. It launches the FIle Chooser.
+     */
+    public static void setupDirectory(){
+
+        JFileChooser fileChooser = new JFileChooser();
+        int result;
+
+        do {
+            fileChooser.setDialogTitle("Select a Place to Save the Files: ");
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+            result = fileChooser.showOpenDialog(new JDialog());
+        } while(result != JFileChooser.APPROVE_OPTION);
+
+        currentDirectory = fileChooser.getSelectedFile();
+    }
+
+    /**
+     * Writes all of our matches to the file for our competition.
+     *
+     * @throws Exception
+     */
+    public static void writeFile() throws Exception{
+        currentFile = new File(currentDirectory, currentCompetition + ".csv");
+
+        ICsvBeanWriter writer = new CsvBeanWriter(new FileWriter(currentFile, false), CsvPreference.STANDARD_PREFERENCE);
+
+        writer.writeHeader(spreadsheetHeaders);
+
+
+        for(Match match : currentMatches){
+            writer.write(match);
+        }
+
+        writer.close();
+    }
+
+    /**
+     * Reads our file for matches so we can access them in the application.
+     * @throws Exception
+     */
+    public static void readFile() throws Exception{
+        currentFile = new File(currentDirectory, currentCompetition + ".csv");
+
+        ICsvBeanReader reader = new CsvBeanReader(new FileReader(currentFile), CsvPreference.STANDARD_PREFERENCE);
+        reader.getHeader(true);
+
+        Match currentMatch;
+
+        while((currentMatch = reader.read(Match.class, null, processors)) != null){
+            currentMatches.add(currentMatch);
+        }
+
+        reader.close();
+    }
 }
--- src/org/toastertech/TTScout/main/Main.java
+++ src/org/toastertech/TTScout/main/Main.java
@@ -1,10 +1,14 @@
 package org.toastertech.TTScout.main;
 
+import org.toastertech.TTScout.data.FileManager;
 import org.toastertech.TTScout.frames.HomeScreen;
+
 
 public class Main {
 
     public static void main(String[] args){
+        FileManager.setupDirectory();
+
         new HomeScreen();
     }
 }
