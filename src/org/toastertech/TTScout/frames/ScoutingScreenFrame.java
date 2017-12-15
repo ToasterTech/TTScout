b6029599e1dfc2c0b4939cdd6ba95b0fad5bb9c7
Jonathan Daniel <gcagrad19@gmail.com>
2017-12-14 23:52:37 -0500

    See Previous Commitgc

--- src/org/toastertech/TTScout/frames/ScoutingScreenFrame.java
+++ src/org/toastertech/TTScout/frames/ScoutingScreenFrame.java
@@ -2,13 +2,16 @@ package org.toastertech.TTScout.frames;
 
 import org.toastertech.TTScout.data.FileManager;
 import org.toastertech.TTScout.data.Match;
+import org.toastertech.TTScout.panels.BrowsingPanel;
 import org.toastertech.TTScout.panels.MatchDetailsPanel;
 import org.toastertech.TTScout.panels.RatingPanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ScoutingScreenFrame extends JFrame{
+public class ScoutingScreenFrame extends JFrame implements ActionListener{
     Match currentMatch;
 
     public ScoutingScreenFrame(Match match){
@@ -24,6 +27,8 @@ public class ScoutingScreenFrame extends JFrame{
 
         add(new MatchDetailsPanel(match));
         add(new RatingPanel(0, 10, "Test Slider"));
+        add(new JLabel());
+        add(new BrowsingPanel(this));
 
         pack();
 
@@ -35,4 +40,32 @@ public class ScoutingScreenFrame extends JFrame{
 
     }
 
+    private void initializeFrame(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()){
+            case "Previous":
+
+                break;
+
+            case "Next":
+
+                break;
+        }
+    }
+
+    private void moveToPreviousMatch(){
+        if(FileManager.currentMatches.indexOf(currentMatch) > 0){
+            //Move to Previous Match
+        }
+    }
+
+    private void moveToNextMatch(){
+        if(FileManager.currentMatches.indexOf(currentMatch) < FileManager.currentMatches.size()-1){
+            //Move to Next Match
+        }
+    }
 }
